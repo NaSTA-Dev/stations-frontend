baf54caeaedf23e11cad3c7dd345826e0578a3af
Luke Moscrop <luke.moscrop@bbc.co.uk>
2016-12-19 17:30:58 +0000

    Added standard as an js linter

--- server/index.js
+++ server/index.js
@@ -39,9 +39,10 @@ app.disable('x-powered-by')
 app.engine('html', Swig.renderFile)
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.static('public'));
 
-app.get('*', function(req, res) {
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
   res.render('index', { 
     vendorjs:  __PROD__ ? assets.vendor.js : '/vendor.js',
     bundlejs: __PROD__ ? assets.main.js : '/main.js'
